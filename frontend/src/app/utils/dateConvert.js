a8ded8b9696be5edce61b991d99f9a44a9fc3e91
Tiago Pesch <tiagopesch@gmail.com>
2025-02-23 18:30:46 -0300

    feat: frontend utils

---
+++ frontend/src/app/utils/dateConvert.js
@@ -0,0 +1,10 @@
+export const isoYearMonthDayMinutes = (string) => {
+  return new Date(string).toLocaleString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+};
---
+++ frontend/src/app/utils/formatCurrencyBRL.js
@@ -0,0 +1,10 @@
+export function formatCurrencyBRL(value) {
+  if (isNaN(value) || value === null) return 'R$ 0,00';
+
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
+}
